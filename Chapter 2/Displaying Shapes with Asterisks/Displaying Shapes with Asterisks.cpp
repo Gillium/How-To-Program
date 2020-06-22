4ca66330d0539310b57712f8c322595757e87396
Peter Piper <petermpiper@gmail.com>
2020-06-22 13:20:45 -0400

    Coded Displaying Shapes with Asterisks program
    Coded Displaying Shapes with Asterisks program. This program prints a box, an oval, an arrow and a diamond.

---
+++ Chapter 2/Displaying Shapes with Asterisks/Displaying Shapes with Asterisks.cpp
@@ -0,0 +1,20 @@
+// Displaying Shapes with Asterisks.cpp : This program prints a box, an oval, an arrow and a diamond
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "*********\t   ***   \t    *    \t    *" << endl;
+	cout << "*       *\t *     * \t   ***   \t   * *" << endl;
+	cout << "*       *\t*       *\t  *****  \t  *   *" << endl;
+	cout << "*       *\t*       *\t    *    \t *     *" << endl;
+	cout << "*       *\t*       *\t    *    \t*       *" << endl;
+	cout << "*       *\t*       *\t    *    \t *     *" << endl;
+	cout << "*       *\t*       *\t    *    \t  *   *" << endl;
+	cout << "*       *\t *     * \t    *    \t   * *" << endl;
+	cout << "*********\t   ***   \t    *    \t    *" << endl;
+
+	return 0;
+}
