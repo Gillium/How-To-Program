f0ac8a67f3abea91926395eb4f6f95577e0b3129
Peter Piper <petermpiper@gmail.com>
2020-06-23 10:48:19 -0400

    Coded Checkerboard Pattern program
    Coded Checkerboard Pattern program. This program displays a checkerboard pattern with eight output statements, then displays the same pattern using as few statements as possible.

---
+++ Chapter 2/Checkerboard Pattern/Checkerboard Pattern.cpp
@@ -0,0 +1,18 @@
+// Checkerboard Pattern.cpp : This program displays a checkerboard pattern with eight output statements, then displays the same pattern using as few statements as possible
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "* * * * * * * *" << endl;
+	cout << " * * * * * * * *" << endl;
+	cout << "* * * * * * * *" << endl;
+	cout << " * * * * * * * *" << endl;
+	cout << "* * * * * * * *" << endl;
+	cout << " * * * * * * * *" << endl;
+	cout << "* * * * * * * *" << endl;
+	cout << " * * * * * * * *" << endl << endl;
+
+	cout << "* * * * * * * *\n * * * * * * * *\n* * * * * * * *\n * * * * * * * *\n* * * * * * * *\n * * * * * * * *\n* * * * * * * *\n * * * * * * * *\n";
+}
